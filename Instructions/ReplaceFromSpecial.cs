a16e5a77ae03ffd7bcf17ef5f61a3161f0617c20
xubiod <xubiod.mail@gmail.com>
2022-02-04 11:35:10 -0500

    two new instructions that overwrite 0xFF

---
+++ Instructions/ReplaceFromSpecial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dumb_lang_test.Instructions
+{
+    internal class ReplaceFromSpecial : Interfaces.IBasicInstruction
+    {
+        public void Execute()
+        {
+            Program.SetMemory(Program.GetMemorySpecial());
+        }
+    }
+}
---
+++ Instructions/ReplaceToSpecial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dumb_lang_test.Instructions
+{
+    internal class ReplaceToSpecial : Interfaces.IBasicInstruction
+    {
+        public void Execute()
+        {
+            Program.SetMemorySpecial(Program.GetMemory());
+        }
+    }
+}
